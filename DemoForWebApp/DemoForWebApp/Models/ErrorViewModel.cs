f1d4dffb57c0b0657588fffc9280729b15752c43
Mohit Gupta <mohit.e.gupta@gmail.com>
2018-05-17 13:16:20 +0000

    First Commit

---
+++ DemoForWebApp/DemoForWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoForWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
